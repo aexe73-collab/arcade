4560978c18d7a98665375a67ba009eaa042792ed
aexe73-collab <aexe73@gmail.com>
2026-03-19 18:54:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: { origin: "*" },
+  pingTimeout: 60000,
+  pingInterval: 25000
+});
 
 const fs = require("fs");
 
